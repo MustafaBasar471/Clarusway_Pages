424102a1e07956c2591317ae13e5472c3ff2a5e6
MustafaBasar471 <mustafabasar471@gmail.com>
2022-09-30 11:26:35 +0300

    weatherapp

---
+++ weather/src/components/WeatherDataHourly.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WeatherDataHourly = (hourlyData) => {
+  return (
+    <div>
+      <div>apofkaopsfkas</div>
+    </div>
+  );
+};
+
+export default WeatherDataHourly;
