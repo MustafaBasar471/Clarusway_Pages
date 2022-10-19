7c9e48ac134803546ed03a8f259b98f46cc9c326
MustafaBasar471 <mustafabasar471@gmail.com>
2022-10-19 14:55:11 +0300

    LocalStorage

--- tasktracker/src/components/Layout.jsx
+++ tasktracker/src/components/Layout.jsx
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, AddTask, Task } from "./";
 
 const Layout = () => {
   const [isActive, setIsActive] = useState(false);
-  const [data, setData] = useState([
-    {
-      id: new Date().getMilliseconds(),
-      title: "The Alpha Team",
-      date: "Dec 12 12.00PM",
-      complated: false,
-    },
-  ]);
+  const [data, setData] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,10 +14,12 @@ const Layout = () => {
       complated: false,
     };
     setData([...data, newTodo]);
+    setTodoToLocalStorage(newTodo);
   };
 
   const handleRemove = (id) => {
     setData(data.filter((todo) => todo.id !== id));
+    deleteTodoToStorage(id);
   };
 
   const handleComplated = (id) => {
@@ -37,6 +32,44 @@ const Layout = () => {
     setData(updatedData);
   };
 
+  // LocalStorage Section
+  const getData = () => {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+  };
+
+  const setTodoToLocalStorage = (newTodo) => {
+    let todo = getData();
+    todo.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify([...todo]));
+  };
+
+  const deleteTodoToStorage = (deleteTodoID) => {
+    let todos = getData();
+
+    todos.forEach((todo, idx) => {
+      if (todo.id === deleteTodoID) {
+        todos.splice(idx, 1);
+      }
+    });
+    localStorage.setItem("todos", JSON.stringify([...todos]));
+  };
+
+  const loadAllData = () => {
+    let todos = getData();
+    setData([...todos]);
+  };
+  useEffect(() => {
+    loadAllData();
+    console.log("This area is working right now");
+  }, []);
+
+  // LocalStoraga Section is Done
   return (
     <div className="mt-[2.5rem] w-[40rem] bg-pink-500 pb-10">
       <Header active={isActive} setActive={setIsActive} />
